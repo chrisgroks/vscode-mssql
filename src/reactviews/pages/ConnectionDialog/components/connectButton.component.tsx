bbb29bdc84de89b8ee30f2ab0fca7c4a55552b80
Benjin Dubishar (from Dev Box) <benjind@microsoft.com>
2025-04-14 14:04:59 -0700

    checkpoint

--- src/reactviews/pages/ConnectionDialog/components/connectButton.component.tsx
+++ src/reactviews/pages/ConnectionDialog/components/connectButton.component.tsx
@@ -3,7 +3,16 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Button, Spinner } from "@fluentui/react-components";
+import {
+    Menu,
+    MenuButtonProps,
+    MenuItem,
+    MenuList,
+    MenuPopover,
+    MenuTrigger,
+    Spinner,
+    SplitButton,
+} from "@fluentui/react-components";
 import { CSSProperties, useContext } from "react";
 import { ConnectionDialogContext } from "./../connectionDialogStateProvider";
 import { ApiStatus } from "../../../../sharedInterfaces/webview";
@@ -25,20 +34,34 @@ export const ConnectButton = ({
     }
 
     return (
-        <Button
-            id={ConnectButtonId}
-            type="submit"
-            appearance="primary"
-            disabled={context.state.connectionStatus === ApiStatus.Loading}
-            className={className}
-            style={style}
-            iconPosition="after"
-            icon={
-                context.state.connectionStatus === ApiStatus.Loading ? (
-                    <Spinner size="tiny" />
-                ) : undefined
-            }>
-            {locConstants.connectionDialog.connect}
-        </Button>
+        <>
+            <Menu positioning="below-end">
+                <MenuTrigger disableButtonEnhancement>
+                    {(triggerProps: MenuButtonProps) => (
+                        <SplitButton
+                            menuButton={triggerProps}
+                            id={ConnectButtonId}
+                            type="submit"
+                            appearance="primary"
+                            disabled={context.state.connectionStatus === ApiStatus.Loading}
+                            className={className}
+                            style={style}
+                            iconPosition="after"
+                            icon={
+                                context.state.connectionStatus === ApiStatus.Loading ? (
+                                    <Spinner size="tiny" />
+                                ) : undefined
+                            }>
+                            {locConstants.connectionDialog.connect}
+                        </SplitButton>
+                    )}
+                </MenuTrigger>
+                <MenuPopover>
+                    <MenuList>
+                        <MenuItem>Test Connection</MenuItem>
+                    </MenuList>
+                </MenuPopover>
+            </Menu>
+        </>
     );
 };
